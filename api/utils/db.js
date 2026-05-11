91ab48574beb87fc461bdc1b2a3275f6bcd11fc1
mr-uzairnaseer <mr.uzairnaseerahmed@gmail.com>
2026-05-11 21:55:58 +0500

    fix: add connection timeout for better error reporting

--- api/utils/db.js
+++ api/utils/db.js
@@ -4,7 +4,9 @@ const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;
   
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+    });
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB Connection Error:', err);
